136b2bfeb4af66ba9d0bde02a5248929195401a3
Servoxi <admin@servoxi.com>
2023-05-23 09:58:53 +0530

    new projects

---
+++ Resources/views/partials/nav.php
@@ -0,0 +1,10 @@
+<div class="menu">
+  <a class="toggleMenu" href="#"><img src="images/nav.png" alt="" /></a>
+	<ul class="nav" id="nav">
+		<li><a href="about">About US</a></li>
+		<li><a href="contact">Contact</a></li>								
+		<li><a href="shop">Products</a></li>
+		<div class="clear"></div>
+	</ul>
+	<script type="text/javascript" src="js/responsive-nav.js"></script>
+</div>		
